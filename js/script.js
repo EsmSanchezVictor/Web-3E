00f33e5e342a1f42dd6f98be952ce983b0bd4024
EsmSanchezVictor <100707976+EsmSanchezVictor@users.noreply.github.com>
2026-04-26 12:52:19 -0300

    menu

--- js/script.js
+++ js/script.js
@@ -188,6 +188,7 @@ document.addEventListener('DOMContentLoaded', () => {
     const mobileMenuBtn = document.getElementById('mobileMenuBtn');
     const navLinks = document.getElementById('navLinks');
 
+    
     if (mobileMenuBtn && navLinks) {
         mobileMenuBtn.addEventListener('click', () => {
             // Abre y cierra la persiana del menú
